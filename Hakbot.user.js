9a93ef54ed5856fbbd102ba402c393ac2ac2b392
anisfencheltee <k1601420@mvrht.net>
2017-08-22 16:24:18 +0200

    Notification for New Article

--- Hakbot.user.js
+++ Hakbot.user.js
@@ -1,7 +1,7 @@
 //==UserScript==
 //@name         RU Bot
 //@namespace    http://tampermonkey.net/
-//@version      1.7.1
+//@version      1.7.2
 //@description  Make RU great Again
 //@updateURL    https://raw.githubusercontent.com/rapupdate/AnisHakbot/master/Hakbot.user.js
 //@downloadURL  https://raw.githubusercontent.com/rapupdate/AnisHakbot/master/Hakbot.user.js
@@ -16,6 +16,7 @@
 //@grant        GM_addStyle
 //@grant        GM_openInTab
 //@grant        GM_xmlhttpRequest
+//@grant        GM_notification
 //==/UserScript==
 
 //=======================================================      
@@ -39,6 +40,7 @@
     GM_addStyle('.helper{cursor:help;}');    
 	GM_addStyle('.articleWarning{background: linear-gradient(to bottom, rgba(202,0,0,1) 0%,rgba(154,10,0,1) 100%);background-image: linear-gradient(rgb(202, 0, 0) 0%, rgb(154, 10, 0) 100%);background-position-x: initial;background-position-y: initial;background-size: initial;background-repeat-x: initial;background-repeat-y: initial;background-attachment: initial;background-origin: initial;background-clip: initial;background-color: initial;text-align:center;color:white;width:97%;padding:10px;margin-left:30px;margin:10px;border-radius:5px 5px 5px 5px;cursor:pointer}');    	
 	GM_deleteValue("error");
+	GM_setValue("onceNotified",false);
 	var FakeLinkChecker = GM_getValue("checkLinks");
 	if (typeof FakeLinkChecker=='undefined'){
 		GM_setValue("checkLinks",confirm("Sollen Rapupdate-Links auf ihre Echtheit überprüft werden?\n\nWenn ihr OK drückt werden XMLHTTP Requests abgesetzt. Wenn das erste Request an eine Domain abgesetzt  wird, erscheint ein Popup von Tampermonkey welches fragt ob der Request zugelassen werden soll.\n\In diesem Popup überprüft die 'ANFRAGEZIEL-DOMAIN', wenn es sich um Rapupdate.de handelt, Klickt auf 'Diese Domain immer zulassen', ansonsten funktioniert der Bot nicht!\n\nDrückt ihr Abbrechen, dann funktioniert alles wie bisher und es werden keine XMLHTTP Requests verschickt!"));
@@ -47,6 +49,11 @@
 	var checkArticle = GM_getValue("checkArticle");
 	if (typeof checkArticle=='undefined'){
 		GM_setValue("checkArticle",confirm("Soll nach neuen Artikeln gesucht werden?\n\nWenn ihr OK drückt werden XMLHTTP Requests abgesetzt. Wenn das erste Request an eine Domain abgesetzt  wird, erscheint ein Popup von Tampermonkey welches fragt ob der Request zugelassen werden soll.\n\In diesem Popup überprüft die 'ANFRAGEZIEL-DOMAIN', wenn es sich um Rapupdate.de handelt, Klickt auf 'Diese Domain immer zulassen', ansonsten funktioniert der Bot nicht!\nWenn ihr den Fakelinkchecker aktiviert habt und dieser funktioniert, dann sollte kein Popup kommen.\n\nDrückt ihr Abbrechen, dann funktioniert alles wie bisher und es werden keine XMLHTTP Requests verschickt!"));
+	}
+	
+	var articleNotification = GM_getValue("articleNotification");
+	if (typeof articleNotification=='undefined'){
+		GM_setValue("articleNotification",true);
 	}
 	
     var clearUrl = GM_getValue("clearUrl");
@@ -123,10 +130,11 @@
 //=======================================================      
 function newArticleBot(){
 	console.log("New Article Bot startet");
+	var articleNotification = GM_getValue("articleNotification");		
 	GM_xmlhttpRequest({
 			method: "GET",
 			url: "http://www.rapupdate.de/api/",
-			onload: function(response) {						
+			onload: function(response) {							
 				var ruApi = response.responseText;
 				var tempDiv = document.createElement('div');
 				tempDiv.innerHTML = ruApi.replace(/<script(.|\s)*?\/script>/g, '');
@@ -145,6 +153,10 @@ function newArticleBot(){
 						GM_openInTab(link,false);
 						console.log("Click");
 					});				
+					if(articleNotification&&!GM_getValue("onceNotified")){
+						GM_setValue("onceNotified",true);
+						GM_notification("Neuer Artikel!","Rapupdate","https://lh3.googleusercontent.com/KRLdUry4tVh571_SDJRU9R6KfaOdnmdWcSIqhmG21KsmA9EdBeL9P7dlJB_HQw6Kqw=w300",function(){GM_openInTab(link,false);});						
+					}
 				}
 
 			},
@@ -161,7 +173,7 @@ function newArticleBot(){
 				}						
 			}
 		});	   
-	setInterval(function(){	
+	setInterval(function(){		
 		GM_xmlhttpRequest({
 			method: "GET",
 			url: "http://www.rapupdate.de/api/",
@@ -184,6 +196,10 @@ function newArticleBot(){
 						GM_openInTab(link,false);
 						//console.log("Click");
 					});				
+					if(articleNotification&&!GM_getValue("onceNotified")){
+						GM_setValue("onceNotified",true);
+						GM_notification("Neuer Artikel!","Rapupdate","https://lh3.googleusercontent.com/KRLdUry4tVh571_SDJRU9R6KfaOdnmdWcSIqhmG21KsmA9EdBeL9P7dlJB_HQw6Kqw=w300",function(){GM_openInTab(link,false);});						
+					}
 				}
 
 			},
@@ -698,6 +714,7 @@ function setInterface(botRunning){
                 var clearUrl = GM_getValue("clearUrl");
                 var reloadTime = GM_getValue("reloadTime");
 				var checkArticle = GM_getValue("checkArticle");
+				var articleNotification = GM_getValue("articleNotification");				
                 reloadTime=reloadTime/60/1000;
 
 				console.log(fakeLinks);
@@ -707,11 +724,17 @@ function setInterface(botRunning){
 					var boxStatusComments = "checked";
 				}else{
 					var boxStatusComments = "";
-				}                
+				}               
+				if(articleNotification){
+					var boxStatusNotify = "checked";
+				}else{
+					var boxStatusNotify = "";
+				}               				
 				if(checkArticle){
 					var boxStatusArticle = "checked";
 				}else{
 					var boxStatusArticle = "";
+					boxStatusNotify+=" disabled";
 				}    
                 if(clearUrl){
 					var boxStatusUrl = "checked";
@@ -743,6 +766,7 @@ function setInterface(botRunning){
                 blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Sneaky Peaky - Zufallszeiten bei Clicks um Menschlich zu wirken"><input type="checkbox" id="natural" '+boxStatusNatural+'><span class="label helper">Natürlicher Modus - zufällige Klickzeiten</span></a><br>';												                
 				blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Nie wieder auf Fake RU-Links reinfallen!"><input type="checkbox" id="fakeBot" '+boxStatus+'><span class="label helper">FakeLinks hervorheben (Benötigt XMLHTTP-Requests)</span></a><br>';										
 				blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Nie wieder zu spät zur Party!"><input type="checkbox" id="checkArticle" '+boxStatusArticle+'><span class="label helper">Auf neue Artikel checken (Benötigt XMLHTTP-Requests)</span></a><br>';										
+				blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Weg mich auf wenn neuer Artikel du hont!"><input type="checkbox" id="notifyArticle" '+boxStatusNotify+'><span class="label helper">Benachrichtigung wenn ein neuer Artikel vorhanden ist (Benötigt Auf neue Artikel checken!)</span></a><br>';										
                 blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Verlinkungen endlich wieder einfach nur eintippen... Puh wie geil ist das denn?"><input type="checkbox" id="clearUrl" '+boxStatusUrl+'><span class="label helper">URLs in Postfähige Form verwandeln</span></a><br>';										
                 blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Es nervt manchmal, aber glaubt mir es ist zu eurem Besten"><input type="checkbox" id="reloadBot" '+boxStatusReload+'><span class="label helper">Reload Bot - Lädt Disqus automatisch neu (Empfohlen!)</span></a><br>';										
                 blacklistDiv.innerHTML += '<a class="dropdown-toggle"  title="Wenn natürlicher Modus an ist, werden bis zu 2 Minuten auf diesen Wert raufgerechnet"><span class="label helper">Reload Interval (In Minuten): </span><input type="Number" id="reloadTime" value="'+reloadTime+'" min="1" max="60"></input><input id="setReloadTime" type="button" value="Speichern" style="margin:20px"></input></a><br>';										
@@ -769,6 +793,10 @@ function setInterface(botRunning){
                         li.addEventListener('click', removeFromClanListOnClick);                                
                     });
                 }
+				
+				$("#notifyArticle").click(function(e){
+					toggleSetting(this,"articleNotification");
+				});
 				$("#fakeBot").click(function(e){
 					toggleSetting(this,"checkLinks");
 				});     
