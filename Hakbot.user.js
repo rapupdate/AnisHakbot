591972d206e5a4f14d77bd3fd801cc88c5741af2
anisfencheltee <k1601420@mvrht.net>
2018-04-04 09:33:17 +0200

    Update Hakbot.user.js

--- Hakbot.user.js
+++ Hakbot.user.js
@@ -1,7 +1,7 @@
 //==UserScript==
 //@name         RU Bot
 //@namespace    http://tampermonkey.net/
-//@version      2.5.2
+//@version      2.6
 //@description  Make RU great Again
 //@updateURL    https://raw.githubusercontent.com/rapupdate/AnisHakbot/master/Hakbot.user.js
 //@downloadURL  https://raw.githubusercontent.com/rapupdate/AnisHakbot/master/Hakbot.user.js
